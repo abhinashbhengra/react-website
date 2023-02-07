c564dbc95c1a17933b4057ef65c40f3719920400
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-02-07 23:26:06 +0530

    row style update

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -65,6 +65,21 @@ export const Section = styled.section`
     padding: ${({ smPadding }) => (smPadding ? smPadding : "70px 0")};
   }
 `;
-export const Row = styled.div``;
+export const Row = styled.div`
+  display: flex;
+  justify-content: ${({ justify }) => (justify ? justify : "")};
+  align-items: ${({ align }) => (align ? align : "")};
+  gap: ${({ gap }) => (gap ? gap : "")};
+  padding: ${({ padding }) => (padding ? padding : "")};
+  margin: ${({ margin }) => (margin ? margin : "")};
+  position: ${({ position }) => (position ? position : "")};
+  width: ${({ width }) => (width ? width : "auto")};
+  min-width: ${({ minWidth }) => (minWidth ? minWidth : "auto")};
+  max-width: ${({ maxWidth }) => (maxWidth ? maxWidth : "auto")};
+  height: ${({ height }) => (height ? height : "auto")};
+  max-height: ${({ maxHeight }) => (maxHeight ? maxHeight : "auto")};
+  min-height: ${({ minHeight }) => (minHeight ? minHeight : "auto")};
+  flex-wrap: ${({ wrap }) => (wrap ? wrap : "")};
+`;
 export const Column = styled.div``;
 export const Button = styled.button``;
