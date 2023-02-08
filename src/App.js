46f9f004e0d656ca01d66c4d0d63127c04bcc982
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-02-08 14:11:03 +0530

    Navbar component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from "react";
+import GlobalStyle from "./globalStyles";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+//Pages
+import Home from "./pages/Home";
+import Pricing from "./pages/PricingPage";
+import Signup from "./pages/SignupPage";
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <Router>
+      <GlobalStyle />
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/pricing" element={<Pricing />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
